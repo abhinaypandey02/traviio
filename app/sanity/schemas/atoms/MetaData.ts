e851d6fb4b0464f4d8ea75bbaeb238aa432eb908
chakri68 <chakridevireddy69@gmail.com>
2023-08-07 17:38:21 +0530

    Added schemas for Metadata and Page

---
+++ app/sanity/schemas/atoms/MetaData.ts
@@ -0,0 +1,31 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'meta_data',
+  title: 'Meta Data',
+  description: 'Meta Data for SEO',
+  type: 'object',
+  options: {
+    collapsible: true,
+  },
+  fields: [
+    defineField({
+      name: 'meta_title',
+      title: 'Meta Title',
+      description: 'Meta title for SEO',
+      type: 'locale_string',
+    }),
+    defineField({
+      name: 'meta_description',
+      title: 'Meta Description',
+      description: 'Meta description for SEO',
+      type: 'locale_string',
+    }),
+    defineField({
+      name: 'meta_image',
+      title: 'Meta Image',
+      description: 'Meta image for SEO',
+      type: 'image',
+    }),
+  ],
+})
