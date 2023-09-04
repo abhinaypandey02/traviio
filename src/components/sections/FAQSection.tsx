4d96ddc2863b11336fd39ad85bdbe3871c8e8a5b
deep-69bits <deepakchattwaniking15@gmail.com>
2023-09-04 12:14:52 +0530

    Testimonial Section added

--- src/components/sections/FAQSection.tsx
+++ src/components/sections/FAQSection.tsx
@@ -15,8 +15,8 @@ const FAQSection = ({ data }: FAQSectionProps) => {
       <h3 className="text-darkblue text-[24px] md:text-[40px] font-[700] leading-[32px] md:leading-[50px] ">
         {data.tagline?.en}
       </h3>
-      <hr className="w-[85px] md:w-[117px] bg-yellow h-1 rounded-[2px] md:rounded-[3px] " />
-      {/* Accordian */}
+      <hr className="w-[85px] md:w-[117px] bg-yellow text-yellow h-1 rounded-full md:rounded-[3px] " />
+    
       <div className="w-full mt-8 space-y-4">
         {data.faqs?.map((faq, index) => (
           <details key={index} className="text-black m-4 md:mx-[80px] ">
