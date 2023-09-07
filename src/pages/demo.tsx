585de0a9069a5de5e882aa91b44c32e310da6cdc
Gaurav Kabra <gauravkab07@gmail.com>
2023-09-07 16:17:20 +0530

    Started Included Tour

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,13 +1,45 @@
-import EnquireForm from '@/components/EnquireForm'
-import Facts from '@/components/Facts'
-import TravelInformation from '@/components/TravelInformation'
 import React from 'react'
+import IncludedTour from '@/components/sections/IncludedTour'
+import Layout from '@/components/layout'
 
 const demo = () => {
   return (
-    <div className='min-h-screen bg-white text-black p-10'>
-      <Facts/>
-    </div>
+    <Layout>
+      <div className='my-10'>
+        <IncludedTour
+          items={[
+            {
+              icon: '/email_icon.svg',
+              title: 'Email',
+              description:
+                '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget augue nec massa volutpat aliquam fringilla non.',
+            },
+            {
+              icon: '/email_icon.svg',
+              title: 'Email',
+              description:
+                '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget augue nec massa volutpat aliquam fringilla non.',
+            },
+            {
+              icon: '/email_icon.svg',
+              title: 'Email',
+              description:
+                '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget augue nec massa volutpat aliquam fringilla non.',
+            },
+            {
+              icon: '/fb_logo.svg',
+              title: 'What\'s included',
+              bullets:[
+                "Helloword",
+                "Hello",
+                "adsfasdfsf",
+                "ASDFdsaf"
+              ]
+            },
+          ]}
+        />
+      </div>
+    </Layout>
   )
 }
 
