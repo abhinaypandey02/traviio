761c6232240676a43e3ea4a78cbb64575ca2b2cf
Yatharth Jain <yatharthjain41@gmail.com>
2023-09-28 03:00:02 +0530

    Added Tailor Your Tour Page

---
+++ src/components/sections/Tailor Your Tour/Step2.tsx
@@ -0,0 +1,128 @@
+import React from 'react'
+
+import { Boat, Car, Compass, Cross, FinnTheHuman, Leaf, WaveTriangle } from '@phosphor-icons/react'
+
+import Input from '@/components/atoms/Input'
+
+export default function Step2() {
+  const [formData, setFormData] = React.useState({
+    name: '',
+    email: '',
+    nationality: '',
+    phone: '',
+    numberOfAdults: '',
+    numberOfChildrens: '',
+    budget: '',
+    categories: [],
+    moreInfo: '',
+  })
+  const CategoriesOptions = [
+    { name: 'Historic Sites', icon: <Leaf /> },
+    { name: 'Religious Sites', icon: <Cross /> },
+    { name: 'Beach', icon: <WaveTriangle /> },
+    { name: 'Desert Safaris', icon: <Car /> },
+    { name: 'Cruises', icon: <Boat /> },
+    { name: 'Outdoor Activities', icon: <Compass /> },
+    { name: 'Wellness Activities', icon: <FinnTheHuman /> },
+  ]
+  const [mobileNumber, setMobileNumber] = React.useState({
+    mobileCode: '+1',
+    mobileNumber: '',
+  })
+  const makeSetValue = (key: keyof typeof formData) => {
+    return (value: any) => {
+      setFormData((prev) => ({
+        ...prev,
+        [key]: value,
+      }))
+    }
+  }
+  const setValue = (value: string, key: string) => {
+    setFormData({ ...formData, [key]: value })
+  }
+  return (
+    <div className="grid lg:grid-cols-2 gap-[18px] px-12">
+      <Input setValue={makeSetValue('name')} value={formData.name} label="Name*" type="text" />
+      <Input setValue={makeSetValue('email')} value={formData.email} label="Email*" type="text" />
+      <Input
+        setValue={makeSetValue('nationality')}
+        value={formData.nationality}
+        label="Nationality*"
+        options={['Indian', 'American']}
+        type="select"
+        placeholder="Select your Nationality"
+      />
+      <div className="flex  font-medium text-base text-black flex-col gap-2">
+        <label htmlFor="mobileNumber">Mobile</label>
+        <div className="border bg-white text-base border-darkblue/10 text-gray rounded p-1 h-8 grid grid-cols-[1fr_7fr] gap-1 divide-x-2 divide-darkblue/10">
+          <input
+            className="min-w-0 w-full flex items-center justify-center h-full overflow-hidden focus:outline-none"
+            id="mobileCode"
+            value={mobileNumber['mobileCode']}
+            onChange={(e) => {
+              setMobileNumber({ ...mobileNumber, mobileCode: e.target.value || '+' })
+              setValue(mobileNumber['mobileCode'] + mobileNumber['mobileNumber'], 'phone')
+              // console.log(mobileNumber)
+            }}
+          />
+          <input
+            className="h-full min-w-0 w-full overflow-hidden focus:outline-none px-2"
+            id="mobileNumber"
+            value={mobileNumber['mobileNumber']}
+            placeholder="Mobile Number"
+            onChange={(e) => {
+              setMobileNumber({ ...mobileNumber, mobileNumber: e.target.value })
+              setValue(mobileNumber['mobileCode'] + mobileNumber['mobileNumber'], 'phone')
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="font-medium text-base text-black">Number of People*</p>
+        <div className="grid sm:grid-cols-2 gap-3">
+          <Input
+            setValue={makeSetValue('numberOfAdults')}
+            value={formData.numberOfAdults}
+            placeholder="Adults"
+            type="buttonNumber"
+          />
+          <Input
+            setValue={makeSetValue('numberOfChildrens')}
+            value={formData.numberOfChildrens}
+            placeholder="Children"
+            type="buttonNumber"
+          />
+        </div>
+      </div>
+      <Input
+        label={
+          <p className="flex gap-[6px] items-center">
+            Your Budget
+            <span className="font-normal text-xs text-gray">(Excluding international flights)</span>
+          </p>
+        }
+        setValue={makeSetValue('budget')}
+        type="select"
+        options={['less than $1000', '$1000-$2000', '$2000-$3000', 'more than $3000']}
+        value={formData.budget}
+      />
+      <div className="col-span-full">
+        <Input
+          setValue={makeSetValue('categories')}
+          value={formData.categories}
+          type="boxSelection"
+          options={CategoriesOptions}
+          label={'Select Categories'}
+        />
+      </div>
+      <div className="col-span-full">
+        <Input
+          type="textarea"
+          label="More Information"
+          setValue={makeSetValue('moreInfo')}
+          value={formData.moreInfo}
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/tailor_your_tour/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+
+import Layout from '@/components/layout'
+import FAQSection from '@/components/sections/FAQSection'
+import Step1 from '@/components/sections/Tailor Your Tour/Step1'
+import Step2 from '@/components/sections/Tailor Your Tour/Step2'
+import Steps from '@/components/sections/Tailor Your Tour/Steps'
+
+export default function index() {
+  const FAQData: any = {
+    faqs: [
+      {
+        question: {
+          _type: 'locale_string',
+          en: 'Can I change or cancel my Tour?',
+        },
+        _type: 'faq',
+        _key: 'f2a8c0373d13',
+        answer: {
+          _type: 'locale_string',
+          en: 'All orders are final once they have been processed through the checkout. Unfortunately, no further changes can be made to your order once processed including and not limited to; removing and/or adding items to your order, combining orders or cancelling your order.Please ensure all details on your order are correct before confirming your checkout. In particular, we recommend double-checking your email address, delivery address and the acceptance, and accuracy, of promotional codes.',
+        },
+      },
+      {
+        _key: 'e01df0a30c7d',
+        answer: {
+          _type: 'locale_string',
+          en: 'Nothing',
+        },
+        question: {
+          _type: 'locale_string',
+          en: 'What if I received my tour and is faulty or incorrect?',
+        },
+        _type: 'faq',
+      },
+      {
+        _type: 'faq',
+        _key: '65facaa04f63',
+        answer: {
+          _type: 'locale_string',
+          en: 'Hello',
+        },
+        question: {
+          _type: 'locale_string',
+          en: 'Do you offer exchanges?',
+        },
+      },
+      {
+        question: {
+          _type: 'locale_string',
+          en: 'Do you offer international tour?',
+        },
+        _type: 'faq',
+        _key: '9c1b457bb047',
+        answer: {
+          _type: 'locale_string',
+          en: 'Hello twice',
+        },
+      },
+      {
+        answer: {
+          _type: 'locale_string',
+          en: 'Jack',
+        },
+        question: {
+          _type: 'locale_string',
+          en: "What should I do if I don't my tour on time?",
+        },
+        _type: 'faq',
+        _key: 'f852a1ce4ed6',
+      },
+    ],
+    _type: 'faq_section',
+    tagline: {
+      _type: 'locale_string',
+      en: 'Frequently asked questions',
+    },
+    _key: 'fdeae1882551',
+    title: {
+      _type: 'locale_string',
+      en: 'FAQ',
+    },
+  }
+  return (
+    <Layout>
+      <Steps>
+        <Step1 />
+        <Step2 />
+      </Steps>
+      <FAQSection data={FAQData} />
+    </Layout>
+  )
+}
