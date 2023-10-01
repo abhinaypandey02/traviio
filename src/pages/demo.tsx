de1f1e00ff8ddcaeda50a7884a86ad62ef7fb27c
Gaurav Kabra <gauravkab07@gmail.com>
2023-10-01 12:35:17 +0530

    Blog refactoring

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -2,25 +2,33 @@ import React from 'react'
 
 import Container from '@/components/Container'
 import Layout from '@/components/layout'
-import BlogDetailCard from '@/components/molecule/BlogDetailCard'
 
-import BlogSidebar from '@/components/organisms/BlogSidebar'
+import BlogChoose from '@/components/molecule/BlogChoose'
 
 const demo = () => {
   return (
     <Layout>
-      <Container className="grid grid-cols-3 my-10">
-        <BlogDetailCard
-          country="India"
-          title="The most interesting historical monuments in Jaipur"
-          date="2021-08-16"
-          image="https://images.unsplash.com/photo-1682686578023-dc680e7a3aeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&q=60"
-          link="/"
-          excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?"
-          author="Deep"
-        />
-        <div></div>
-        <BlogSidebar />
+      <Container className="">
+        <BlogChoose
+          items={[
+            {
+              images: ['/calendar.svg'],
+              title: 'Palestine',
+              link: '/blogs/palestine',
+            },
+
+            {
+              images: ['/calendar.svg'],
+              title: 'Calendar2',
+              link: '/',
+            },
+            {
+              images: ['/calendar.svg', '/calendar.svg'],
+              title: 'Calendar',
+              link: '/demo',
+            },
+          ]}
+        ></BlogChoose>
       </Container>
     </Layout>
   )
